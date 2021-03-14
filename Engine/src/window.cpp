608f23943b3b7cdc7f84a2ca5a3225800f784b39
osyrta <skyrta00@gmail.com>
2021-03-14 14:33:33 +0100

    log opengl errors in debug mode

--- Engine/src/window.cpp
+++ Engine/src/window.cpp
@@ -8,6 +8,20 @@
 
 namespace ce {
 
+	void GLAPIENTRY
+		MessageCallback(GLenum source,
+			GLenum type,
+			GLuint id,
+			GLenum severity,
+			GLsizei length,
+			const GLchar* message,
+			const void* userParam)
+	{
+		fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+			(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+			type, severity, message);
+	}
+
 	Window* current_window = nullptr;
 	
 	Window::Window(int width, int height, const char* name) {
@@ -22,10 +36,6 @@ namespace ce {
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); 
 
-#ifndef _DEBUG
-		glfwWindowHint(GLFW_DOUBLEBUFFER, GL_FALSE);
-#endif // !_DEBUG
-
 		window = glfwCreateWindow(width, height, name, nullptr, nullptr);
 		glfwMakeContextCurrent(window);
 				
@@ -38,6 +48,11 @@ namespace ce {
 		int fbWidth, fbHeight;
 		glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
 		glViewport(0, 0, fbWidth, fbHeight);
+
+#ifdef _DEBUG
+		glEnable(GL_DEBUG_OUTPUT);
+		glDebugMessageCallback(MessageCallback, 0);
+#endif
 
 		renderTarget.load(width, height);
 	}
